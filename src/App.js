14021d83cc22e590688079ac1542c1c9288bc97c
k2037451 <thomas.godfrey97@yahoo.com>
2024-01-30 23:48:55 +0000

    edit message

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Also Hi.
+          To get started, edit <code>src/App.js</code> and save to reload. goodbye.
         </p>
       </div>
     );
